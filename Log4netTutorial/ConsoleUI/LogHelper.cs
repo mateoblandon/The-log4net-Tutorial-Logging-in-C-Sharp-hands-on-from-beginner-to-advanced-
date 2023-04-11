acbb9ec60b35baca9229818667bd16b467b6ed12
Mateo Blandon Mesa <matius-b@hotmail.com>
2023-04-11 09:31:54 -0500

    I created the project, downloaded log4net NuGet package, set up the App.config file, added assembly, created log reference, first log.

---
+++ Log4netTutorial/ConsoleUI/LogHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleUI
+{
+  public class LogHelper
+  {
+    public static log4net.ILog GetLogger([CallerFilePath]string filename = "")
+      {
+        return log4net.LogManager.GetLogger(filename);
+      }
+  }
+}
---
+++ Log4netTutorial/ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+namespace ConsoleUI
+{
+  class Program
+  {
+    public static readonly log4net.ILog log = LogHelper.GetLogger(); //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello wolrd");
+      log.Error("This is my error message");
+      Console.ReadLine();
+    }
+  }
+}
