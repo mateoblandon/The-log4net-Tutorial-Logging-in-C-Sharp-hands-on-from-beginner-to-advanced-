4011045459c9da281b72735c4688e26293668247
Mateo Blandon Mesa <matius-b@hotmail.com>
2023-04-13 09:44:08 -0500

    I Added a filter log in the App.config file and modified the conversionPattern.

--- Log4netTutorial/ConsoleUI/Program.cs
+++ Log4netTutorial/ConsoleUI/Program.cs
@@ -13,8 +13,13 @@ namespace ConsoleUI
     public static readonly log4net.ILog log = LogHelper.GetLogger(); //log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello wolrd");
-      log.Error("This is my error message");
+      log.Debug("Developer: tutorial was fun");
+      log.Info("Maintenance: water pump turned on");
+      log.Warn("Maintenance: water pump is getting hot");
+      var i = 0;
+      try { var x = 10 / i; } 
+      catch (DivideByZeroException ex) {  log.Error("Developer: we tried to divide by zero again", ex); }
+      log.Fatal("Maintenance: water pump exploded");
       Console.ReadLine();
     }
   }
